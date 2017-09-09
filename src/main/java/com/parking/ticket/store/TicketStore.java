f4902d0a697c68df2aff4223f04d7ca91a1bbefd
Sathish Raman <ramans@Sathishs-MacBook-Pro.local>
2017-09-09 11:46:14 +0530

    ticket store interface to abstract the dao/in memory operations from the ticket manager

--- src/main/java/com/parking/ticket/store/TicketStore.java
+++ src/main/java/com/parking/ticket/store/TicketStore.java
@@ -1,6 +1,14 @@
 package com.parking.ticket.store;
 
-public class TicketStore {
+
+/**
+ * TicketStore acts as in memory store for managing tickets in the system.
+ * This store can be replaced with dao layer in  required.
+ * @author ramans
+ *
+ */
+public class TicketStore implements TicketStoreInterface{
+
 
 	
 	
---
+++ src/main/java/com/parking/ticket/store/TicketStoreInterface.java
@@ -0,0 +1,5 @@
+package com.parking.ticket.store;
+
+public interface TicketStoreInterface {
+
+}
